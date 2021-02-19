2d397cd9e28b789e3614d47b3c6b778a63bc5b77
Poojyeswar <poojyesh2002@gmail.com>
2021-02-19 11:12:23 +0530

    Initial commit

---
+++ about.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("ENter a , b values" );
+	scanf("%d%d",&a,&b);
+	c = a+b;
+	printf("THe sum of 2 numbers is  c  = :",c);
+
+}
